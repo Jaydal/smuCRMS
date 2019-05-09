b506d6db118c0677845e21ffeb880c69e3a35691
Marie Chantal Seenivasagam <mcseenivasagam@gmail.com>
2019-05-09 10:03:34 +0800

    Added Login Form

---
+++ smuCRMS/View/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smuCRMS.View
+{
+    public partial class Login : MaterialSkin.Controls.MaterialForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialSingleLineTextField1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
